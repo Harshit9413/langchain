54b2077b3287b1e3661b20fdde04bcf6afe3e533
VISHAL JANGID <vishaljangid@VISHALs-MacBook-Air.local>
2026-04-08 15:11:37 +0530

    first

--- p.py
+++ p.py
@@ -1,57 +1,53 @@
-# ✅ Hide all warnings
-import warnings
-warnings.filterwarnings("ignore")
+from functools import partialmethod
 
-# ✅ Hide transformers logs
-from transformers import logging
-logging.set_verbosity_error()
+from tqdm import tqdm
+from transformers import logging as tf_logging
 
-# ✅ Hide HF hub logs
+tqdm.__init__ = partialmethod(tqdm.__init__, disable=True)
+tf_logging.set_verbosity_error()
 import os
-os.environ["HF_HUB_DISABLE_PROGRESS_BARS"] = "1"
+
+from dotenv import load_dotenv
 from langchain_community.document_loaders import PyPDFLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_groq import ChatGroq
 from langchain_huggingface import HuggingFaceEmbeddings
-from dotenv import load_dotenv
-load_dotenv()
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
+load_dotenv(dotenv_path=".env")
 loader = PyPDFLoader("sample_research_paper.pdf")
 documents = loader.load()
-
 text_splitter = RecursiveCharacterTextSplitter(
     chunk_size=800,
     chunk_overlap=150
 )
 docs = text_splitter.split_documents(documents)
-embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+
+embeddings = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+)
 db = FAISS.from_documents(docs, embeddings)
 retriever = db.as_retriever(search_kwargs={"k": 5})
-llm = ChatGroq(model="llama-3.1-8b-instant")
-print("📄 Chatbot Ready!\n")
 
+llm = ChatGroq(
+    model="llama-3.1-8b-instant",
+    groq_api_key=os.getenv("GROQ_API_KEY")
+)
+print("📄 Chatbot Ready!\n")
 while True:
     query = input("Ask: ")
-
     if query.lower() == "exit":
         break
 
     results = retriever.invoke(query)
-
     context = "\n\n".join([doc.page_content for doc in results])
-
-    prompt = f"""
-You are an intelligent assistant.
-
-Answer the question ONLY using the provided context.- Be clear and detailed.- If not found, say "Answer not found in document".
+    prompt = f"""You are an intelligent assistant.
+Answer the question ONLY using the provided context.- Be clear and detailed. If not found, say "Answer not found in document".
 Context:
 {context}
 Question:
 {query}
-Answer:
-"""
+Answer:"""
     response = llm.invoke(prompt)
-
     print("\n🤖", response.content)
-  
+    print("\n" + "─" * 60 + "\n")
